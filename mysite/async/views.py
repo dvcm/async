73b90c5c1862bf8a8fe7c54d0531bf204c77e7a6
dvcm <pshaggy@gmail.com>
2022-10-20 13:08:13 -0400

    Exercicio Proposto

--- mysite/async/views.py
+++ mysite/async/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import asyncio
+import httpx
 
-# Create your views here.
+
+async def http_call_async():
+    for num in range(1, 6):
+        await asyncio.sleep(1)
+        print(num)
+    async with httpx.AsyncClient() as client:
+        r = await client.get("https://httpbin.org")
+        print(r)
